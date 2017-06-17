7f815e276e026757cff5e04324262f882121a528
Prasanth Jayachandran <prasanthj@apache.org>
2017-06-16 17:49:20 -0700

    script to convert sql files to csv. added all 99 tpcds queries

---
+++ sqlfiles-to-csv.py
@@ -0,0 +1,15 @@
+import os,sys
+
+directory = sys.argv[1]
+for filename in os.listdir(directory):
+	if filename.endswith(".sql"):
+		queryname = os.path.basename(filename)
+		with open(filename) as f:
+			lines = f.readlines()
+			# remove comments from lines, use, set commands
+			nocomments = filter(lambda v: not (v.strip().startswith("--") or v.strip().lower().startswith("use") or v.strip().lower().startswith("set")), lines)
+			# remove line endings
+			nolinebreaks = map(lambda v: v.strip().lower(), nocomments)
+			joinedquery = " ".join(nolinebreaks).strip()
+			query = joinedquery.rstrip(';')
+		print queryname.strip() + "^" + query
