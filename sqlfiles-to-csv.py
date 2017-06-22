68d4c06e82251ceebc049707083e99ca10e6063b
Prasanth Jayachandran <prasanthj@apache.org>
2017-06-21 17:33:50 -0700

    all 99 queries derived from structor trivial rewrites

--- sqlfiles-to-csv.py
+++ sqlfiles-to-csv.py
@@ -4,7 +4,7 @@ directory = sys.argv[1]
 for filename in os.listdir(directory):
 	if filename.endswith(".sql"):
 		queryname = os.path.basename(filename)
-		with open(filename) as f:
+		with open(os.path.join(directory,filename)) as f:
 			lines = f.readlines()
 			# remove comments from lines, use, set commands
 			nocomments = filter(lambda v: not (v.strip().startswith("--") or v.strip().lower().startswith("use") or v.strip().lower().startswith("set")), lines)
