fec98f60c843fd728fdc413d893a3dbd75ebc9be
Prasanth Jayachandran <prasanthj@apache.org>
2017-07-13 17:10:08 -0700

    ats download loop fix

--- ats-download.sh
+++ ats-download.sh
@@ -3,7 +3,7 @@
 DAG_PREFIX=$1
 DAG_IDX_FROM=$2
 DAG_IDX_TO=$3
-for i in {$DAG_IDX_FROM..$DAG_IDX_TO}; 
+for (( i=$DAG_IDX_FROM; i<=$DAG_IDX_TO; i++ ))
 do
   echo "Downloading ATS data for ${DAG_PREFIX}_${i} .."; 
   time hadoop jar tez-debug-jars/tez-debugtool-0.0.1-SNAPSHOT.jar org.apache.tez.tools.debug.Main --dagId ${DAG_PREFIX}_${i} --sourceType HIVE_ATS --sourceType TEZ_ATS; 
